bd5bb21ccb151a31accbda16183259c580bdb0c9
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-11 08:55:13 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main: Size is not grandeur
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of char:%d byte(s)\n",size of (a)));
+	printf("Size of int:%d byte(s)\n",size of (b)));
+	printf("Size of long int:%d byte(s)\n",size of (c)));
+	printf("Size of long long int:%d byte(s)\n",size of (d)));
+	printf("Size of float:%d byte(s)\n",size of (e)));
+	retun (0);
+}
