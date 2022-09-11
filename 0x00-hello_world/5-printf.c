b0b606361adb3f0f6c4dabd0773ad4fb49dd74f1
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-11 08:38:44 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,6 @@
 /**
  * Main: Print with the function (printf)
  *
- * Description: Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
- *
  * Return: 0 (Successful)
  */
 int main(void)
