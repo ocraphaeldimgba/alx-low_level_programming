26be9cbbf1a0830b952f98c3b02702ced9fc3ef1
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-22 02:53:25 -0700

    1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints positive ot negative integer
+ * return - 0
+ */
+
+int main(void)
+
+{
+	int i;
+	i = 0;
+	positive or negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+
+
+void positive or negative(int i);
+int largest number(int a, int b, int c);
+void print remaining days(int month, int day, int year);
+int convet day (int month, int day);
+
+#endif /* MAIN_H */
