9612faaa57750ecf739b069ec4f9f6d8e10132ff
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-23 11:09:01 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function to add char
+ * @dest: first char
+ * @src: second char
+ * @n: third char
+ * Return: String
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *scr, n);
+	return (dest);
+}
