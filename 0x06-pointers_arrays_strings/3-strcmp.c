2f1c5b85c685e10f09e15f223ecd44fbac3e08aa
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-23 11:48:44 -0700

    4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - for comparing
+ * @s1: char1
+ * @s2: char2
+ * Return: whole number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
