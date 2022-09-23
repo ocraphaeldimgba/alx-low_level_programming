0a755635b8179c4f9614d458997736591f2b7aa7
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-23 14:07:58 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,8 +7,8 @@
  */
 char *leet(char *x)
 {
-	int a = 0; b = 0; l = 5;
-	char tr[5] = ['A', 'E', 'O', 'T', 'L'};
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0' '7', '1'};
 
 	while (x[a])
