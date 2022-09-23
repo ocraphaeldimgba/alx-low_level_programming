7db721c7db89d1b76ff4a4ae9bd06d8de7605ebb
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-23 06:32:07 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c - character checks
  * Return - returns 0 or 1 depending on conditions
  */
-int _isalpha(int c)
+int _isalphabet(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
