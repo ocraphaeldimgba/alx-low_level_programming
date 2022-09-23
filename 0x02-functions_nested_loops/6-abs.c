a446f8a4cd1581d446c0fa42f359eab0c97d81b3
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-23 07:00:12 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * _abs(int) - computes absolute value
+ * @n - checks for absolute value
+ * Return - 1
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	n = -n;
+	return (n);
+}
+		
