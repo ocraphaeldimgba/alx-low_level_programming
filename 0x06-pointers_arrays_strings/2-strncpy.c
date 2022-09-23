33f5dcdc453a85d2b008fc7561759e22350cfa76
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-09-23 11:36:26 -0700

    3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,15 @@
+#include "main.h"
+#include <string.h>
 
+/**
+ * _strncpy - function for copy
+ * @dest: char1
+ * @src: char2
+ * @n: char3
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
