518bd3bccb961f20b3c4024b7d1d6f5c9536fbdf
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-11-04 01:37:17 -0700

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - function that copies
+ * @dest: copy to
+ * @src: in the string s
+ * @n: number to copy
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
+
+	while (n-- > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (dest);
+}
