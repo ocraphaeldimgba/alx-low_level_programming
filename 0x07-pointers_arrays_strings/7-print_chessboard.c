f8ce5a02debaa2ab4ebeedfac61cf3bfd7c6b512
ocraphaeldimgba <ocraphaeldimgba@gmail.com>
2022-11-04 03:05:35 -0700

    h

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,13 +4,14 @@
 /**
  * print_chessboard - Write a function that prints the chessboard
  * @a: print
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
 	int x;
 	int y;
 
-	for (x = 0; x < 0; x++)
+	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
 		{
